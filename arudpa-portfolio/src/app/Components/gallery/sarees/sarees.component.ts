07fd33045cf685bfb6a65bd9678d78230f815a6b
ashifkareem17-arr <ashifkareem17@gmail.com>
2026-09-19 08:26:11 +0530

    Update sarees collection and add Arudpa favicon

--- arudpa-portfolio/src/app/Components/gallery/sarees/sarees.component.ts
+++ arudpa-portfolio/src/app/Components/gallery/sarees/sarees.component.ts
@@ -15,16 +15,12 @@ export class SareesComponent {
     { id: 4, name: 'Lime yellow with parrot green border saree', price: 999, image: '/saree 4.png' },
     { id: 5, name: 'Peocock blue with purple border saree', price: 999, image: '/saree 5.png' },
     { id: 6, name: 'Dark Purple with emarald green border saree', price: 999, image: '/saree 6.png' },
-    { id: 7, name: 'Half Saree', price: 999, image: '/half saree 7.png' },
-    { id: 8, name: 'Half saree', price: 999, image: '/half saree 8.png' },
-    { id: 9, name: 'Half saree', price: 999, image: '/half saree 9.png' },
-    { id: 10, name: 'Half saree', price: 999, image: '/half saree 10.png' },
-    { id: 11, name: 'Half saree', price: 999, image: '/half saree 11.png' },
-    { id: 12, name: 'Half saree', price: 999, image: '/half saree 12.png' }
-    
-    
-    
-    
+    { id: 7, name: 'Golden yellow half saree with magenta-gold border', price: 1200, image: '/half saree 7.png' },
+    { id: 8, name: 'Bright orange half saree with maroon-gold border', price: 1200, image: '/half saree 8.png' },
+    { id: 9, name: 'Crimson red half saree with black-gold border', price: 1200, image: '/half saree 9.png' },
+    { id: 10, name: 'Peacock blue half saree with hot pink-gold border', price: 1200, image: '/half saree 10.png' },
+    { id: 11, name: 'Parrot green half saree with royal blue-gold border', price: 1200, image: '/half saree 11.png' },
+    { id: 12, name: 'Deep purple half saree with turquoise-gold border', price: 1200, image: '/half saree 12.png' }    
 
 
     
